6464b281e24f61532d3c05e2fe82c79bfde08aa0
Dotuananh13 <bimtrum182@gmail.com>
2021-11-05 09:50:19 +0700

    tuaanh

---
+++ data.js
@@ -0,0 +1,31 @@
+var xe =
+`[
+ {
+   "id": 1,
+   "name": "ABC",
+   "price": 1000000,
+   "description": "<p>tuan anh dep trai</p>",
+   "imageName": "xe-1"
+ },
+ {
+   "id": 2,
+   "name": "DEF",
+   "price": 2000000,
+   "description": "<p>tuan anh ngao</p>",
+   "imageName": "xe-2"
+ },
+ {
+   "id": 3,
+   "name": "GHI",
+   "price": 3000000,
+   "description": "<p>bim dai ca</p>",
+   "imageName": "xe-3"
+ },
+ {
+   "id": 4,
+   "name": "JKL",
+   "price": 4000000,
+   "description": "<p>bim trum</p>",
+   "imageName": "xe-4"
+ }
+]`;
